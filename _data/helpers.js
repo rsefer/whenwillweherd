0c01dfb623301fab1aeba1bf380c2e85b0933d2f
Robert Sefer <rsefer@gmail.com>
2021-02-02 11:09:28 -0600

    Cleanup

--- _data/helpers.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const UglifyJS = require('uglify-js');
-const sass = require('node-sass');
-
-module.exports = {
-	js: UglifyJS.minify(fs.readFileSync('./_src/scripts/main.js', 'utf8')).code
-};
